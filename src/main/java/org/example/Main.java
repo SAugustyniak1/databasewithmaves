a80b646aed68eef08ab97f00728eaeacf27eaeaa
Augustyniak Sebastian <Sebastian.Augustyniak@t-mobile.pl>
2020-12-21 15:38:36 +0100

    data base with select and insert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,45 @@
 package org.example;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
+
+        Scanner scanner = new Scanner(System.in);
+        //(city, phone, addressLine1, country, postalCode, territory)
+        System.out.println("What is your city");
+        String city = scanner.nextLine();
+
+        System.out.println("What is your phone");
+        String phone = scanner.nextLine();
+
+        System.out.println("What is your adress line");
+        String addressLine1 = scanner.nextLine();
+
+        System.out.println("What is your countery");
+        String countrey = scanner.nextLine();
+
+        System.out.println("What is your postal code");
+        String postalCode = scanner.nextLine();
+
+        System.out.println("What is your territory");
+        String territory = scanner.nextLine();
+
+
+
+
+        exampleUpdate(city,phone,addressLine1, countrey, postalCode,territory);
+
+        exampleSelect();
+
+
+
+    }
+
+    public static void exampleSelect() throws Exception{
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/classicmodels", "root", "123456");
 
         System.out.println(connection);
@@ -24,9 +55,37 @@ public class Main {
 
 
         while(result.next()){
+            long officeCode = result.getLong("officeCOde");
             city = result.getString("city");
-            System.out.println(city);
+            System.out.println(city + " " + officeCode);
         }
+        connection.close();
+
 
     }
+
+    public static void exampleUpdate(String city, String phone, String adressLine1, String country, String postalCode, String territory) throws Exception{
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/classicmodels", "root", "123456");
+
+        System.out.println(connection);
+
+//        String values = String.format("VALUES (%s,  %s, %s, %s, %s)", city, phone, adressLine1, postalCode, territory);
+//
+//        System.out.println(values);
+
+        String command = String.format("INSERT INTO offices (city, phone, addressLine1, country, postalCode, territory)\n" +
+                "VALUES ('%s',  '%s', '%s', '%s', '%s', '%s')", city, phone, adressLine1, country, postalCode, territory);
+
+        System.out.println(command);
+
+        Statement statement = connection.createStatement();
+
+
+        int affectedRows = statement.executeUpdate(command);
+
+       connection.close();
+
+    }
+
+
 }
